a1335e2cb8d503d9f07bcc320b34fe11cbf25599
JeongSeop <tjqtjq13@gmail.com>
2024-06-19 21:10:47 +0900

    240619

---
+++ TryCatch/TryCatch/MainApp.cs
@@ -0,0 +1,24 @@
+﻿namespace TryCatch
+{
+    internal class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine($"예외가 발생했습니다 : {e.Message}");
+            }
+
+            Console.WriteLine("쫑료");
+        }
+    }
+}
